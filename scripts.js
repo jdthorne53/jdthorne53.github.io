5979b720539d20259776e90339b879e906fd18a4
jdthorne53 <101368633+jdthorne53@users.noreply.github.com>
2023-10-04 15:06:54 +1100

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,5 @@
+// Add interactivity as needed
+document.querySelector("#contact form").addEventListener("submit", function(e) {
+    e.preventDefault(); // Prevent the form from submitting for demonstration purposes
+    alert("Form submitted!");
+});
