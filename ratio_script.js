38f4985c6ee63edac0f120210f621dbea7cca505
jdthorne53 <101368633+jdthorne53@users.noreply.github.com>
2023-10-17 13:59:19 +1100

    updates

--- ratio_script.js
+++ ratio_script.js
@@ -1,50 +1,49 @@
 let currentPosition = 0;
-let difficulty = "easy"; // default difficulty
+let currentAnswer = "";
 
-let riddles = {
-    //easy
-    1: [
-        { question: "If there are 5 apples and 10 bananas, what's the ratio of apples to bananas?", answer: "1:2" },
-        { question: "If there are 4 cats and 8 dogs, what's the ratio of cats to dogs?", answer: "1:2" },
-        { question: "If there are 3 pencils and 6 erasers, what's the ratio of pencils to erasers?", answer: "1:2" },
-        { question: "If there are 2 oranges and 3 apples, what's the ratio of oranges to apples?", answer: "2:3" },
-        { question: "If there are 7 boys and 14 girls in a class, what's the ratio of boys to girls?", answer: "1:2" },
-        { question: "If there are 6 birds and 3 nests, what's the ratio of birds to nests?", answer: "2:1" },
-        { question: "If there are 9 chocolates and 3 kids, what's the ratio of chocolates to kids?", answer: "3:1" },
-        { question: "If there are 8 cars and 4 garages, what's the ratio of cars to garages?", answer: "2:1" },
-        { question: "If there are 10 pencils and 5 notebooks, what's the ratio of pencils to notebooks?", answer: "2:1" },
-        { question: "If there are 3 balls and 9 players, what's the ratio of balls to players?", answer: "1:3" }
-    ],
-    //hard
-    2: [
-        { question: "If there are 7 apples and 21 bananas, what's the ratio of apples to bananas?", answer: "1:3" },
-        { question: "If there are 5 cats and 15 dogs, what's the ratio of cats to dogs?", answer: "1:3" },
-        { question: "If there are 13 lions and 39 zebras, what's the ratio of lions to zebras?", answer: "1:3" },
-        { question: "If there are 8 oranges and 24 apples, what's the ratio of oranges to apples?", answer: "1:3" },
-        { question: "If there are 11 boys and 33 girls in a class, what's the ratio of boys to girls?", answer: "1:3" },
-        { question: "If there are 9 birds and 27 trees, what's the ratio of birds to trees?", answer: "1:3" },
-        { question: "If there are 14 chocolates and 42 kids, what's the ratio of chocolates to kids?", answer: "1:3" },
-        { question: "If there are 6 cars and 18 garages, what's the ratio of cars to garages?", answer: "1:3" },
-        { question: "If there are 10 pencils and 30 notebooks, what's the ratio of pencils to notebooks?", answer: "1:3" },
-        { question: "If there are 4 balls and 12 players, what's the ratio of balls to players?", answer: "1:3" }
-    ]
-};
+function generateRandomQuestion() {
+    let objects = ['apples', 'bananas', 'cats', 'dogs', 'pencils', 'erasers', 'oranges', 'apples', 'boys', 'girls', 'birds', 'nests', 'chocolates', 'kids', 'cars', 'garages', 'pencils', 'notebooks', 'balls', 'players'];
+    let obj1 = objects[Math.floor(Math.random() * objects.length)];
+    let obj2 = objects[Math.floor(Math.random() * objects.length)];
+    let num1 = Math.floor(Math.random() * 10) + 1; // Random number between 1 and 10
+    let multipliers = [1.5, 2, 2.5, 3, 3.5, 4, 4.5, 5];
+    let multiplier = multipliers[Math.floor(Math.random() * multipliers.length)];
+    let num2 = Math.round(num1 * multiplier); // num2 is a product of num1 and the multiplier
+    let gcdValue = gcd(num1, num2); // Greatest common divisor
+
+    // Randomly decide which number will be larger
+    if (Math.random() > 0.5) {
+        [num1, num2] = [num2, num1];
+        [obj1, obj2] = [obj2, obj1];
+    }
+
+    let ratio = `${num1/gcdValue}:${num2/gcdValue}`;
+    return {
+        question: `If there are ${num1} ${obj1} and ${num2} ${obj2}, what's the ratio of ${obj1} to ${obj2}?`,
+        answer: ratio
+    };
+}
+
+function gcd(a, b) {
+    if (!b) {
+        return a;
+    }
+    return gcd(b, a % b);
+}
+
 
 function loadRiddle() {
-    if (currentPosition < riddles[difficulty].length) {
-        document.querySelector('.riddle').innerText = "Riddle: " + riddles[difficulty][currentPosition].question;
-    } else {
-        alert('You completed the ' + difficultyname + ' mode!');
-        resetGame();
-    }
+    let riddle = generateRandomQuestion();
+    document.querySelector('.riddle').innerText = "Riddle: " + riddle.question;
+    currentAnswer = riddle.answer; // Store the answer for checking later
 }
 
 function checkAnswer() {
     let answer = document.getElementById('answer').value;
-    if (answer === riddles[difficulty][currentPosition].answer) {
-        currentPosition++;
+    if (answer === currentAnswer) {
         document.getElementById('position').innerText = currentPosition;
         loadRiddle();
+        currentPosition++;
     } else {
         alert('Try again!');
     }
